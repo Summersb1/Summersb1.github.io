00ff8443940ad948adee1b8f69faf5a1bab00d1e
Summersb1 <somersbyappl@gmail.com>
2024-11-04 13:49:20 +0900

    test

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Summersb1',
-  bio: 'Hello World',
+  bio: 'test bio',
   links: [
     {
       name: 'Ko-fi',
